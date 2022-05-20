035a0d4b52989dc0b5aa27d5a99d6c32cec7da56
robotdognz <robotdognz@gmail.com>
2022-05-21 10:30:32 +1200

    Added basic player controls and water system

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float landRunSpeed = 5;
+    [SerializeField] float landJumpPower = 10;
+    [SerializeField] float landGravity = 5;
+
+    [SerializeField] float waterRunSpeed = 5;
+    [SerializeField] float waterJumpPower = 10;
+    [SerializeField] float waterGravity = 5;
+
+    bool isInWater = false;
+    float runSpeed;
+    float jumpPower;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    Collider2D myCollider;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<CapsuleCollider2D>();
+
+        myRigidBody.gravityScale = landGravity;
+    }
+
+    private void Run()
+    {
+        if (!isInWater)
+        {
+            // do land movement
+            Vector2 playerVelocity = new Vector2(moveInput.x * landRunSpeed, myRigidBody.velocity.y);
+            myRigidBody.velocity = playerVelocity;
+        }
+        else
+        {
+            // do water movement
+
+            float yMovement = Mathf.Clamp(myRigidBody.velocity.y + moveInput.y * waterRunSpeed, -10, 10);
+
+            Vector2 playerVelocity = new Vector2(moveInput.x * waterRunSpeed, yMovement);
+            myRigidBody.velocity = playerVelocity;
+        }
+    }
+
+    void Update()
+    {
+        UpdateTerranType();
+        Run();
+    }
+
+    private void UpdateTerranType()
+    {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Water")))
+        {
+            runSpeed = landRunSpeed;
+            jumpPower = landJumpPower;
+            myRigidBody.gravityScale = landGravity;
+            isInWater = false;
+        }
+        else
+        {
+            runSpeed = waterRunSpeed;
+            jumpPower = waterJumpPower;
+            myRigidBody.gravityScale = waterGravity;
+            isInWater = true;
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Platforms", "Water")))
+        {
+            return;
+        }
+
+        if (value.isPressed)
+        {
+            myRigidBody.velocity += new Vector2(0, jumpPower);
+
+            // myRigidBody.
+        }
+    }
+}
