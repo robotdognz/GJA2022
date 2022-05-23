491454a13b70bf263d8e10c7a1a2c580e31c7aec
robotdognz <robotdognz@gmail.com>
2022-05-23 18:36:55 +1200

    Working on merging music into the general audio manager. Ambiances now start at random point

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,28 +4,48 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Music Mixer")]
+    [SerializeField][Range(0f, 1f)] float menuMusicVolume = 1f;
+    [SerializeField][Range(0f, 1f)] float gameMusicVolume = 1f;
 
-    [Header("Mixer")]
+    [Header("Player Mixer")]
     [SerializeField][Range(0f, 1f)] float jumpVolume = 1f;
     [SerializeField][Range(0f, 1f)] float footstepsVolume = 1f;
     [SerializeField][Range(0f, 1f)] float intoWaterVolume = 1f;
     [SerializeField][Range(0f, 1f)] float outOfWaterVolume = 1f;
-    [SerializeField][Range(0f, 1f)] float warningVolume = 1f;
-    [SerializeField][Range(0f, 1f)] float gameOverVolume = 1f;
+
+    [Header("Ambiance Mixer")]
     [SerializeField][Range(0f, 1f)] float normalAmbianceVolume = 1f;
     [SerializeField][Range(0f, 1f)] float waterAmbianceVolume = 1f;
 
-    [Header("Clips")]
+    [Header("UI Mixer")]
+    [SerializeField][Range(0f, 1f)] float warningVolume = 1f;
+    [SerializeField][Range(0f, 1f)] float gameOverVolume = 1f;
+    [SerializeField][Range(0f, 1f)] float UIClickVolume = 1f;
+    [SerializeField][Range(0f, 1f)] float UIHoverVolume = 1f;
+
+    [Header("Music Clips")]
+    [SerializeField] AudioClip menuMusicClip;
+    [SerializeField] AudioClip gameMusicClip;
+
+    [Header("Player Clips")]
     [SerializeField] AudioClip[] jumpClips;
-    [SerializeField] AudioClip footstepsClip;
     [SerializeField] AudioClip[] intoWaterClips;
     [SerializeField] AudioClip[] outOfWaterClips;
-    [SerializeField] AudioClip[] warningClips;
-    [SerializeField] AudioClip[] gameOverClips;
+    [SerializeField] AudioClip footstepsClip;
+
+    [Header("Ambiance Clips")]
     [SerializeField] AudioClip normalAmbianceClip;
     [SerializeField] AudioClip waterAmbianceClip;
-    
+
+    [Header("UI Clips")]
+    [SerializeField] AudioClip[] warningClips;
+    [SerializeField] AudioClip[] gameOverClips;
+    [SerializeField] AudioClip[] UIClickClips;
+    [SerializeField] AudioClip[] UIHoverClips;
+
     [Header("Sources")]
+    [SerializeField] AudioSource music;
     [SerializeField] AudioSource ambiance;
     [SerializeField] AudioSource jumpSource;
     [SerializeField] AudioSource footsteps;
@@ -33,38 +53,90 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource intoWaterSource;
     [SerializeField] AudioSource warningSource;
     [SerializeField] AudioSource gameOverSource;
+    [SerializeField] AudioSource UISource;
 
-    // --------- one shots -----------------
+    // --------- Music -----------------
+
+    // TODO
+
+
+    // --------- Player -----------------
 
     public void PlayJumpClip()
     {
-        // PlayClipAtPoint(jumpClips, jumpVolume);
         PlayClipWithSource(jumpClips, jumpVolume, jumpSource);
     }
 
     public void PlayIntoWaterClip()
     {
-        // PlayClipAtPoint(intoWaterClips, intoWaterVolume);
         PlayClipWithSource(intoWaterClips, intoWaterVolume, intoWaterSource);
     }
 
     public void PlayOutOfWaterClip()
     {
-        // PlayClipAtPoint(outOfWaterClips, outOfWaterVolume);
         PlayClipWithSource(outOfWaterClips, outOfWaterVolume, outOfWaterSource);
     }
 
+    public void StartFootsteps()
+    {
+        footsteps.clip = footstepsClip;
+        footsteps.volume = footstepsVolume;
+        footsteps.time = Random.value * footstepsClip.length;
+        footsteps.Play();
+    }
+
+    public void StopFootsteps()
+    {
+        footsteps.Stop();
+    }
+
+
+    // ---------- Ambiance ----------------
+
+    private void Start()
+    {
+        StartNormalAmbiance();
+    }
+    public void StartNormalAmbiance()
+    {
+        ambiance.clip = normalAmbianceClip;
+        ambiance.volume = normalAmbianceVolume;
+        ambiance.time = Random.value * normalAmbianceClip.length;
+        ambiance.Play();
+    }
+
+    public void StartWaterAmbiance()
+    {
+        ambiance.clip = waterAmbianceClip;
+        ambiance.volume = waterAmbianceVolume;
+        ambiance.time = Random.value * waterAmbianceClip.length;
+        ambiance.Play();
+    }
+
+
+    // --------- UI -----------------
+
     public void PlayWarningClip()
     {
-        // PlayClipAtPoint(warningClips, warningVolume);
         PlayClipWithSource(warningClips, warningVolume, warningSource);
     }
 
     public void PlayGameOverClip()
     {
-        // PlayClipAtPoint(gameOverClips, gameOverVolume);
         PlayClipWithSource(gameOverClips, gameOverVolume, gameOverSource);
     }
+
+    public void PlayUIClickClip()
+    {
+        PlayClipWithSource(UIClickClips, UIClickVolume, UISource);
+    }
+
+    public void PlayUIHoverClip()
+    {
+        PlayClipWithSource(UIHoverClips, UIHoverVolume, UISource);
+    }
+
+    // ----------- Helper ----------------
 
     private void PlayClipAtPoint(AudioClip[] clips, float volume)
     {
@@ -87,40 +159,6 @@ public class AudioManager : MonoBehaviour
             source.volume = volume;
             source.Play();
         }
-    }
-
-    // ---------- loops ----------------
-
-    private void Start() {
-        StartNormalAmbiance();
-    }
-    public void StartNormalAmbiance()
-    {
-        ambiance.clip = normalAmbianceClip;
-        ambiance.volume = normalAmbianceVolume;
-        ambiance.time = Random.value * normalAmbianceClip.length;
-        ambiance.Play();
-    }
-
-    public void StartWaterAmbiance()
-    {
-        ambiance.clip = waterAmbianceClip;
-        ambiance.volume = waterAmbianceVolume;
-        ambiance.time = Random.value * waterAmbianceClip.length;
-        ambiance.Play();
-    }
-
-    public void StartFootsteps()
-    {
-        footsteps.clip = footstepsClip;
-        footsteps.volume = footstepsVolume;
-        footsteps.time = Random.value * footstepsClip.length;
-        footsteps.Play();
-    }
-
-    public void StopFootsteps()
-    {
-        footsteps.Stop();
     }
 
 }
